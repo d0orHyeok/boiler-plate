6f92c07ca85a4bf51809c47d308a7d3c9d9f9041
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-25 08:20:54 +0900

    Add login page

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-function LoginPage() {
-    return <div>LoginPage</div>;
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+
+    // 이메일 입력
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    };
+    // 패스워드 입력
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password,
+        };
+
+        dispatch(loginUser(body)).then((response) => {
+            if (response.payload.loginSuccess) {
+                props.history.push("/");
+            } else {
+                alert("Error");
+            }
+        });
+    };
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+            }}
+        >
+            <form onSubmit={onSubmitHandler} style={{ display: "flex", flexDirection: "column" }}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,25 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css"; // for CSS
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider
+            store={createStoreWithMiddleware(
+                Reducer,
+                // For redux devtools
+                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+        >
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
