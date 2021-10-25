65d942f2180e5c624934ad71c8b1e5384206191f
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-10-25 09:03:12 +0900

    Add: 로그아웃 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,26 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
     useEffect(() => {
         axios.get("/api/hello").then((response) => console.log(response.data));
     }, []);
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`).then((response) => {
+            if (response.data.success) {
+                props.history.push("/login");
+            } else {
+                alert("로그아웃 하는데 실패 했습니다.");
+            }
+        });
+    };
 
     return (
         <div
             style={{
                 display: "flex",
+                flexDirection: "column",
                 justifyContent: "center",
                 alignItems: "center",
                 width: "100%",
@@ -17,6 +28,8 @@ function LandingPage() {
             }}
         >
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>로그아웃</button>
         </div>
     );
 }
